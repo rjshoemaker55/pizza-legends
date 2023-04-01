6da56bfde893a61e6bcbf3b5a535c269c1aa6e53
RJ Shoemaker <raymond.shoemaker@spigroup.com>
2023-03-31 21:33:47 -0400

    video 6 - camera movement

--- Overworld.js
+++ Overworld.js
@@ -7,23 +7,36 @@ class Overworld {
   }
 
   startGameLoop() {
+    const fps = 60;
+    let now;
+    let then = Date.now();
+    let interval = 1000 / fps;
+    let delta;
+
     const step = () => {
       // Clear canvas
       this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-      // Draw Lower layer
-      this.map.drawLowerImage(this.ctx);
+      // Establish the camera person
+      const cameraPerson = this.map.gameObjects.hero;
 
-      // Draw game objects
+      // Update all objects
       Object.values(this.map.gameObjects).forEach((object) => {
         object.update({
           arrow: this.directionInput.direction
         });
-        object.sprite.draw(this.ctx);
+      });
+
+      // Draw Lower layer
+      this.map.drawLowerImage(this.ctx, cameraPerson);
+
+      // Draw all objects
+      Object.values(this.map.gameObjects).forEach((object) => {
+        object.sprite.draw(this.ctx, cameraPerson);
       });
 
       // Draw upper layer
-      this.map.drawUpperImage(this.ctx);
+      this.map.drawUpperImage(this.ctx, cameraPerson);
 
       requestAnimationFrame(() => {
         step();
